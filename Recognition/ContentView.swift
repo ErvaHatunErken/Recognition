f509765c0ffdc0e67aa37ecad8672c9a5e67a641
Erva Hatun Tekeoğlu <eherken@gmail.com>
2021-09-13 19:52:43 +0300

    Initial Commit

---
+++ Recognition/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Recognition
+//
+//  Created by Erva Hatun Tekeoğlu on 13.09.2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Recognition/RecognitionApp.swift
@@ -0,0 +1,17 @@
+//
+//  RecognitionApp.swift
+//  Recognition
+//
+//  Created by Erva Hatun Tekeoğlu on 13.09.2021.
+//
+
+import SwiftUI
+
+@main
+struct RecognitionApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
