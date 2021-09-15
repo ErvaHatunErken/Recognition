7dbb635e6b0bf510b60dc208d36332660e022dd6
Erva Hatun Tekeoğlu <eherken@gmail.com>
2021-09-15 22:17:50 +0300

    code cleaning -take image part is removed. Image scanning part is kept.

--- Recognition/CameraView.swift
+++ Recognition/CameraView.swift
@@ -11,16 +11,13 @@ import VisionKit
 import Vision
 
 struct CameraView: UIViewControllerRepresentable {
-    //@Binding var selectedImage: UIImage
     @Binding var showCameraView: Bool
     @Environment(\.presentationMode) private var presentationMode
     @Binding var recognizedText: String
     
-    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraView>) -> VNDocumentCameraViewController /*UIImagePickerController */{
-        let cameraVC = VNDocumentCameraViewController() //UIImagePickerController()
+    func makeUIViewController(context: UIViewControllerRepresentableContext<CameraView>) -> VNDocumentCameraViewController {
+        let cameraVC = VNDocumentCameraViewController()
         cameraVC.delegate = context.coordinator
-       // cameraVC.sourceType = .camera
-        //cameraVC.allowsEditing = false
         return cameraVC
     }
 
@@ -28,10 +25,10 @@ struct CameraView: UIViewControllerRepresentable {
         Coordinator(self, $recognizedText)
     }
     
-    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController /*UIImagePickerController */, context: UIViewControllerRepresentableContext<CameraView>) {
+    func updateUIViewController(_ uiViewController: VNDocumentCameraViewController, context: UIViewControllerRepresentableContext<CameraView>) {
     }
     
-    final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate /*UIImagePickerControllerDelegate, UINavigationControllerDelegate */{
+    final class Coordinator: NSObject, VNDocumentCameraViewControllerDelegate {
      
         var parent: CameraView
         var recognizedText: Binding<String>
@@ -39,14 +36,6 @@ struct CameraView: UIViewControllerRepresentable {
             self.parent = parent
             self.recognizedText = recognizedText
         }
-     /*
-        func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
-            if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-                self.parent.selectedImage = image
-                //parent.showCameraView = false
-            }
-            self.parent.presentationMode.wrappedValue.dismiss()
-        }*/
         
         func documentCameraViewController(_ controller: VNDocumentCameraViewController, didFinishWith scan: VNDocumentCameraScan) {
             let extractedImages = extractImages(from: scan)
@@ -54,10 +43,6 @@ struct CameraView: UIViewControllerRepresentable {
             recognizedText.wrappedValue = processedText
             self.parent.presentationMode.wrappedValue.dismiss()
         }
-        
-        /*func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
-            self.parent.showCameraView = false
-        }*/
         
         fileprivate func extractImages( from scan: VNDocumentCameraScan) -> [CGImage] {
             var extractedImages = [CGImage]()
@@ -80,7 +65,6 @@ struct CameraView: UIViewControllerRepresentable {
                 let maximumRecognitionCandidates = 1
                 for observation in observations {
                     guard let candidate = observation.topCandidates(maximumRecognitionCandidates).first else { continue }
-                    //entireRecognizedText += "\(candidate.string)\n"
                     entireRecognizedText.append(candidate.string.lowercased())
                 }
             }
--- Recognition/ContentView.swift
+++ Recognition/ContentView.swift
@@ -8,7 +8,6 @@
 import SwiftUI
 
 struct ContentView: View {
-    //@State private var selectedImage = UIImage()
     @State private var showCameraView: Bool = false
     @State private var recognizedText = "Take a photo to start scanning."
     var body: some View {
@@ -39,35 +38,7 @@ struct ContentView: View {
     .navigationBarTitle("Text Recognition")
     .sheet(isPresented: $showCameraView) {
         CameraView(showCameraView: self.$showCameraView, recognizedText: self.$recognizedText)
-    }
-     /*
-        VStack {
-            Image(uiImage: selectedImage)
-                .resizable()
-                .scaledToFill()
-                .frame(minWidth: 0, maxWidth: .infinity)
-            Button(action: {
-                self.showCameraView = true
-            }) {
-                HStack {
-                    Image(systemName: "camera")
-                        .font(.system(size: 24))
-                    Text("Take a photo")
-                        .font(.headline)
-                }
-                .frame(minWidth: 0, maxWidth: .infinity, minHeight: 0, maxHeight: 50)
-                .background(Color.blue)
-                .foregroundColor(.white)
-                .cornerRadius(20)
-                .padding(.horizontal)
-            }
-        }
-        .sheet(isPresented: $showCameraView) {
-            CameraView(selectedImage: self.$selectedImage, showCameraView: self.$showCameraView)
-        }*/
-    }
-    
-    
+    } 
 }
 
 struct ContentView_Previews: PreviewProvider {
